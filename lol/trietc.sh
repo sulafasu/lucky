5a3af991b1f99be3ff8cafa9f33df6d776652ee9
sulafasu <81042800+sulafasu@users.noreply.github.com>
2021-03-27 02:56:19 +0700

    Update trietc.sh

--- lol/trietc.sh
+++ lol/trietc.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-POOL=eu.ezil.me:5555
-WALLET=zil1zm8gh7a7zh7d64rk7vn30gkhxnq90mdtgwm43j
-WALLET1=0xF2214d927e6323191a2E1217ab1792d366d368bc
-WALLET2=0xA4Ad0cc5e03bE67ac56bA8AbB0eedDE4Ee739C79
+ETHPOOL=eth.cruxpool.com:5555
+ETHWALLET=0xF2214d927e6323191a2E1217ab1792d366d368bc.lucky
 
-POOL1=eu1-etc.ethermine.org:4444
-WORKER=lucky
+ETCPOOL=eu1-etc.ethermine.org:4444
+ETCWALLET=0xA4Ad0cc5e03bE67ac56bA8AbB0eedDE4Ee739C79.block
 
 cd "$(dirname "$0")"
 
-./lol --algo ETHASH --pool $POOL --user $WALLET1.$WALLET --worker $WORKER  $@ --enablezilcache --dualmode zil --dualstratum --algo ETCHASH $WALLET2@$POOL1
-
+./lolMiner --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --dualmode etc --dualstratum $ETCWALLET@$ETCPOOL  $@
