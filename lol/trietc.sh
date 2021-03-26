6d025ee074124482f8aca53aaca6f785700fec71
sulafasu <81042800+sulafasu@users.noreply.github.com>
2021-03-27 06:35:13 +0700

    Update trietc.sh

--- lol/trietc.sh
+++ lol/trietc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ETHPOOL=eth.cruxpool.com:5555
-ETHWALLET=0xF2214d927e6323191a2E1217ab1792d366d368bc.lucky
+ETHWALLET=lucky.lucky
 
 ETCPOOL=eu1-etc.ethermine.org:4444
 ETCWALLET=0xA4Ad0cc5e03bE67ac56bA8AbB0eedDE4Ee739C79.block
