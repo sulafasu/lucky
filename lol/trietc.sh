254200a68ba4e1e3cd6938483c395d14c375a844
sulafasu <81042800+sulafasu@users.noreply.github.com>
2021-03-27 08:11:32 +0700

    Update trietc.sh

--- lol/trietc.sh
+++ lol/trietc.sh
@@ -4,9 +4,9 @@ ETHPOOL=eth.cruxpool.com:5555
 ETHWALLET=lucky
 
 ETCPOOL=eu1-etc.ethermine.org:4444
-ETCWALLET=0xA4Ad0cc5e03bE67ac56bA8AbB0eedDE4Ee739C79
+ETCWALLET=0xA4Ad0cc5e03bE67ac56bA8AbB0eedDE4Ee739C79.lucky
 WORKER=lucky
 
 cd "$(dirname "$0")"
 
-./lol --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --worker $WORKER --dualmode etc --dualstratum $ETCWALLET.$WORKER@$ETCPOOL
+./lol --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --worker $WORKER --dualmode etc --dualstratum $ETCWALLET@$ETCPOOL
